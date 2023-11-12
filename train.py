783fd774918436272f9c7797de4ca01db65beb9d
Ethan <ethantam@berkeley.edu>
2023-11-11 21:25:12 -0800

    dataloader final touches

--- train.py
+++ train.py
@@ -1,51 +1,30 @@
 import torch
-from torch_geometric.loader import DataLoader
 import json
-import glob
 import os
 import skimage.io as io
 import numpy as np
 
 class PSFDataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir):
-        self.data = glob.glob(data_dir)
+    # input files : list of volumes
+    # gt_files : list of json files with the lls_defocus_offset value
+    def __init__(self, input_files, gt_files):
+        self.input_files = input_files
+        self.gt_files = gt_files
 
     def __len__(self):
-        return len(self.data)
+        return len(self.input_files)
     
     def __getitem__(self, idx):
-        photons = os.listdir(self.data_dir)
-        for photon in photons:
-            photons_path = os.path.join(self.data_dir, photon)
-            photons_path = os.path.join(photons_path, "amp_p0-p0")
-            defocused_psfs = os.listdir(photons_path)
-            for defocused_psf in defocused_psfs:
-                psf_path = os.path.join(photons_path, defocused_psf)
-                files = os.listdir(psf_path)
-                files = sorted(files) # sort file names
-                for i in range(0, len(files) // 2, 2):
-                    json_file, tif_file = files[i], files[i+1]
-                    image = io.imread(tif_file)
-                    with open(json_file,'r') as j :
-                        sample_load_file=json.load(j)
-                        lls_offset = sample_load_file["lls_defocus_offset"]
-                        return lls_offset, image
-
-
-train_path="/clusterfs/nvme/ethan/dataset/lls_defocus_only/YuMB_lambda510/z200-y108-x108/z64-y64-x64/z15/mixed"
-
-train_dataset = PSFDataset(train_path)
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, follow_batch=['x_s', 'x_t'])
-
+        image = io.imread(self.input_files[idx])
+        with open(self.gt_files[idx],'r') as j:
+            load_json = json.load(j)
+            lls_offset = load_json["lls_defocus_offset"]
+            return image, lls_offset
 
 # this should only return the dataloader for ONLY ONE defocus_xxx_xxx
 def dataloader(path, batch_size, val_split):
     # path = the path inside one defocus_xxx_xxx
     data = sorted(os.listdir(path))
-    num_data = len(data)
-    num_val = int(num_data * val_split)
-    num_train = num_data - num_val
-
     input_files = []
     gt_files = []
     for file in data:
@@ -56,24 +35,30 @@ def dataloader(path, batch_size, val_split):
     input_files = np.array(input_files)
     gt_files = np.array(gt_files)
 
+    num_data = len(input_files)
     idx = np.arange(num_data)
     np.random.shuffle(idx)
-    train_idx = idx[:num_train]
-    train_input_files = input_files[train_idx]
-    train_gt_files = gt_files[train_idx]
-    print('Training size: ', len(train_input_files))
+    num_val = int(num_data * val_split)
+    num_train = num_data - num_val
 
-    train_data = PSFDataset(train_input_files, train_gt_files)
+    train_idx = idx[:num_train]
+    train_input_filenames = input_files[train_idx]
+    train_gt_filenames = gt_files[train_idx]
+    print('Training size: ', len(train_input_filenames))
+
+    train_data = PSFDataset(train_input_filenames, train_gt_filenames)
     train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
     val_idx = idx[num_train:]
     val_input_filenames = input_files[val_idx]
     val_gt_filenames = gt_files[val_idx]
+    print('Validation size: ', len(val_input_filenames))
+
+    val_data = PSFDataset(val_input_filenames, val_gt_filenames)
+    val_dataloader = torch.utils.data.DataLoader(val_data, batch_size=batch_size, shuffle=True)
+
+    return train_dataloader, val_dataloader
     
-
-
-
-
 
 def train(input_path):
     photons = os.listdir(input_path)
@@ -83,30 +68,15 @@ def train(input_path):
         defocused_psfs = os.listdir(photons_path)
         for defocused_psf in defocused_psfs:
             psf_path = os.path.join(photons_path, defocused_psf)
-            files = os.listdir(psf_path)
-            files = sorted(files) # sort file names
-            for i in range(0, len(files) // 2, 2):
-                json_file, tif_file = files[i], files[i+1]
-                image = io.imread(tif_file)
-                with open(json_file,'r') as j :
-                    sample_load_file=json.load(j)
-                    lls_offset = sample_load_file["lls_defocus_offset"]
-                    return lls_offset, image
+            train_dataloader, val_dataloader = dataloader(psf_path, 1)
+            
+            for image, lls_offset in train_dataloader:
+                pass
 
-
-
-
-
-
-    for photons in glob.glob(train_path):
-        photons_path = os.path.join(train_path, photons)
-        photons_path = os.path.join(photons_path, "amp_p0-p0")
-        train_dataset = PSFDataset(photons_path)
-        train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, follow_batch=['x_s', 'x_t'])
-        for json, tif in train_loader:
-            pass
-
-    # train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=bsize, shuffle=True, pin_memory=pmem)
+            for image, lls_offset in val_dataloader:
+                pass
 
 if __name__ == '__main__':
+    train_path="/clusterfs/nvme/ethan/dataset/lls_defocus_only/YuMB_lambda510/z200-y108-x108/z64-y64-x64/z15/mixed"
+    train(train_path)
     pass
