edcd25c6f566fe01e7ea7d8e56d2b605b2688a0d
Ethan <ethantam@berkeley.edu>
2023-11-18 15:41:43 -0800

    projections

--- projections.py
+++ projections.py
@@ -49,7 +49,7 @@ def main():
     params = parameters()
 
     # Replace 'your_file.tif' with the path to your 3D TIF file
-    file_path = '/clusterfs/nvme/ethan/dataset/aberrations/20.tif'
+    file_path = '/clusterfs/nvme/ethan/dataset/aberrations/21.tif'
     match = re.search(r'\d+', file_path)
     file_num = int(match.group()) - 1
 
