21d8cff9be5e8225a1975853dfb42edf1ae98f1b
Ethan <ethantam@berkeley.edu>
2023-12-26 20:18:15 -0800

    test

--- projections.py
+++ projections.py
@@ -89,7 +89,7 @@ def main():
     #params = parameters()
     params = test_parameters()
 
-    for i in range(1,2):
+    for i in range(2,3):
         
         #file_path = f'/clusterfs/nvme/ethan/dataset/aberrations/{i}.tif'
         file_path = f'/clusterfs/nvme/ethan/dataset/test/{i}.tif'
