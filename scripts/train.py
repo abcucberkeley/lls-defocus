153133014f33b2d6ec17640bea1c12472f4e9549
Ethan Tam <ethantam@berkeley.edu>
2024-02-08 01:59:29 -0800

    debug

--- scripts/train.py
+++ scripts/train.py
@@ -145,6 +145,11 @@ def train_no_amp(input_path, n_epochs, model_path, experiment_name):
         # write to csv file
         with open(f'../experiments/{experiment_name}.csv', 'a', newline='') as f: # i changed from w to a, to append just check again
             writer = csv.writer(f)
+            train_loss = train_total_loss / len(train_dataloader)
+            val_loss = val_total_loss / len(val_dataloader)
+
+            print(train_loss)
+            print(val_loss)
             writer.writerow([(train_total_loss / len(train_dataloader)).detach().numpy()[0], (val_total_loss / len(val_dataloader)).detach().numpy()[0]])
         
         # update loss graph
