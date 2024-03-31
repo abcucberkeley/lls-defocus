544fcecf9f3654cbe5bdcef9d811d5c32cc7bff5
Ethan Tam <ethantam@berkeley.edu>
2024-03-31 15:54:35 -0700

    loss

--- scripts/loss_graph.py
+++ scripts/loss_graph.py
@@ -1,5 +1,7 @@
 import csv
 from matplotlib.pylab import plt
+import pandas as pd
+import sys
 
 def plot_loss(experiment_name, epoch):
     epochs = []
@@ -23,3 +25,29 @@ def plot_loss(experiment_name, epoch):
     # if epoch % 10 == 0 and epoch != 0:
     #     plt.show()
     plt.savefig(f'../experiments/{experiment_name}.png')
+
+
+def plot(experiment_name, csv_file):
+    # Read the data from the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+
+    # Plot the data
+    df.plot(y=["Training Loss", "Validation Loss"], marker='o')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title(f'Training and Validation Loss of {experiment_name}')
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: python plot_function.py experiment_name csv_file")
+        sys.exit(1)
+
+    # Extract experiment_name and csv_file from command-line arguments
+    experiment_name = sys.argv[1]
+    csv_file = sys.argv[2]
+
+    # Call the plot function
+    plot(experiment_name, csv_file)
