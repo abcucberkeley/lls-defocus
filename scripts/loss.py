8af11d1884bccd3299611cf777720bdb1a82f227
Ethan Tam <ethantam@berkeley.edu>
2024-04-05 16:22:05 -0700

    loss

--- scripts/loss.py
+++ scripts/loss.py
@@ -9,7 +9,7 @@ class Custom_MAE(nn.Module):
     def forward(self, y_pred, y_true):
         # if difference is greater than the threshold, penalize more
         abs_diff = torch.abs(y_pred - y_true)
-        small_dev_mask = abs_diff >= self.threshold and abs_diff < 1
+        small_dev_mask = (abs_diff >= self.threshold) & (abs_diff < 1)
 
         # penalize for all abs_diff that are greater than the threshold 
         # to ensure penalization of small (yet still significant) deviations from true value
