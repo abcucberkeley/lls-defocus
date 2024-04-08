0762bb7cbf219ded622f9c0b27ccf1aba28ca4dc
Ethan Tam <ethantam@berkeley.edu>
2024-04-07 23:12:26 -0700

    check

--- scripts/loss.py
+++ scripts/loss.py
@@ -7,7 +7,7 @@ class Custom_MAE(nn.Module):
         self.threshold = threshold
 
     def forward(self, y_pred, y_true):
-        has_nan_pred = torch.isnan(y_pred).any()
+        has_nan_pred = torch.isnan(y_pred).any() # hm already has nan.. prob has to do with 
         has_nan_true = torch.isnan(y_true).any()
         assert not has_nan_pred, "Tensor contains NaN values"
         assert not has_nan_true, "Tensor contains NaN values"
@@ -18,6 +18,7 @@ class Custom_MAE(nn.Module):
 
         # penalize for all abs_diff that are greater than the threshold 
         # to ensure penalization of small (yet still significant) deviations from true value
-        loss = torch.where(small_dev_mask, 1 / torch.add(abs_diff, 1e-6), torch.pow(abs_diff,2)) # avoid dividing by zero
+        #loss = torch.where(small_dev_mask, 1 / torch.add(abs_diff, 1e-6), torch.pow(abs_diff,2)) # avoid dividing by zero
+        loss = torch.where(small_dev_mask, torch.add(abs_diff, 2), torch.pow(abs_diff,2)) # avoid dividing by zero
         print(loss)
         return torch.mean(loss)
